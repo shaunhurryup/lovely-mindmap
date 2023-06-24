27fbdc890fb160fd862055467a463c9535aecd61
邵绅宸搞快点 <shenchen.shao@js.design>
2023-06-24 22:26:50 +0800

    * create child node by Tab shortcut

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor, ItemView, KeymapEventHandler, KeymapEventListener, MarkdownRenderChild,
+	MarkdownView,
+	Menu,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	WorkspaceLeaf
+} from 'obsidian'
 
 // Remember to rename these classes and interfaces!
 
@@ -10,19 +21,135 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+const VIEW_TYPE_EXAMPLE = "example-view";
+
+class ExampleView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Example view";
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("h4", { text: "Example view" });
+	}
+
+	async onClose() {
+		// Nothing to clean up.
+	}
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	canvas: any;
+	hShortcut: KeymapEventHandler;
+	tabShortcut: KeymapEventHandler;
+	cShortcut: KeymapEventHandler;
+
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+		await this.app.workspace.getRightLeaf(false).setViewState({
+			type: VIEW_TYPE_EXAMPLE,
+			active: true,
+		});
+
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+		);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.canvas = app.workspace.getActiveViewOfType(ItemView)?.canvas
+
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		);
+
+		this.cShortcut = this.app.scope.register([], 'c', () => {
+			// const selections = this.canvas.selection
+			// if (selections.size !== 1) return
+
+		})
+
+		const random = (e: number) => {
+			let t = [];
+			for (let n = 0; n < e; n++) {
+				t.push((16 * Math.random() | 0).toString(16));
+			}
+			return t.join("")
+		}
+
+		this.tabShortcut = this.app.scope.register([], 'Tab', () => {
+			const selections = this.canvas.selection
+			if (selections.size !== 1) return
+
+			const parentNode = selections.values().next().value
+
+			const {
+				x,
+				y,
+				width,
+				height,
+			} = parentNode
+
+			const childNode = this.canvas.createTextNode({
+				pos: {
+					x: x + width + 200,
+					y: y,
+					height: height,
+					width: width
+				},
+				size: {
+					x: x + width + 200,
+					y: y,
+					height: height,
+					width: width
+				},
+				text: "",
+				focus: false,
+				save: true,
+			})
+
+			this.canvas.addNode(childNode)
+
+			const data = this.canvas.getData()
+
+			this.canvas.importData({
+				"edges": [
+					...data.edges,
+					{
+						"id": random(6),
+						"fromNode": parentNode.id,
+						"fromSide": 'right',
+						"toNode": childNode.id,
+						"toSide": 'left',
+					}
+				],
+				"nodes": data.nodes,
+			})
+
+			this.canvas.requestFrame();
+		})
+
+		this.hShortcut = this.app.scope.register([], 'h', () => {
+			console.log('canvas => \n\n', this.canvas)
+		})
+
+		this.addRibbonIcon("dice", "Activate view", () => {
+			this.activateView();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -36,50 +163,24 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		this.app.scope.unregister(this.hShortcut)
+		this.app.scope.unregister(this.tabShortcut)
+		this.app.scope.unregister(this.cShortcut)
 	}
 
 	async loadSettings() {
@@ -121,6 +222,8 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+		containerEl.createEl("h1", { text: "Heading 1" });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
